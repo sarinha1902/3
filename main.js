fd56c9af8e5e9f14bfe3a446cdd00b3a1c217cfd
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-03-25 08:17:33 -0300

    att

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorA11(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick =function(){
+
+        for(let j=0;j<botoes.length;)
+
+        botoes[i].classList.add("ativo")
+    }
+    console.log( );
+}
