3ac643657654eb6a4085b91beb34e4aee2d491c8
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-04-01 08:44:26 -0300

    att

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const botoes = document.querySelectorA11(".botao");
+const textos = document.querySelectorAll()
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick =function(){
 
-        for(let j=0;j<botoes.length;)
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
 
-        botoes[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
     }
-    console.log( );
+
 }
