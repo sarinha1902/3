6590b03de8f9e29bcb9e07f044b898d8c693f2c4
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-04-15 08:13:43 -0300

    att

--- main.js
+++ main.js
@@ -23,5 +23,8 @@ function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
-    return segundos;
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    return horas;
 }
